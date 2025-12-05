fac774710c590d5c1780e80f98f60cebe4a5fbdf
MKhansa067 <105094399+MKhansa067@users.noreply.github.com>
2025-12-05 23:29:02 +0700

    Update DashboardService.java

--- src/main/java/projectapps/kalkulatorhpp/services/DashboardService.java
+++ src/main/java/projectapps/kalkulatorhpp/services/DashboardService.java
@@ -8,7 +8,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.OptionalDouble;
 
-//Anggota 3 (Dashboard)
+//Anggota 3: Ghevyra (Dashboard)
 public class DashboardService {
     private final UserData ud;
 
